ec224909a6b1af4e334f892f5bd9e2bd26716a71
caloré <72320103+Caloor@users.noreply.github.com>
2023-06-06 10:03:10 +0200

    neo
    neo

---
+++ index.php
@@ -0,0 +1,340 @@
+<?php require 'header.php'; ?>
+
+  
+    <div id="content-wrapper">
+
+       
+        <section id="home" class="full-height px-lg-5">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-10">
+                        <h1 class="display-4 fw-bold" data-aos="fade-up">Fausta Benjamin<span class="text-brand">
+                                Etudiant</span> à l'ETNA</h1>
+                        <p class="lead mt-2 mb-4" data-aos="fade-up" data-aos-delay="300">Je suis Fausta Benjamin, étudiant à l'Etna, à la recherche d'une alternance</p>
+                        <div data-aos="fade-up" data-aos-delay="600">
+                            <a href="#work" class="btn btn-brand me-3">Voir mes projets</a>
+                            <a href="#" class="link-custom">Me joindre: 07 68 77 99 53. Paris</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+      
+        <section id="services" class="full-height px-lg-5">
+            <div class="container">
+
+                <div class="row pb-4" data-aos="fade-up">
+                    <div class="col-lg-8">
+                        <h6 class="text-brand">Ce que je peux faire pour vous</h6>
+                        <h1>Voici ce que je propose</h1>
+                    </div>
+                </div>
+
+                <div class="row gy-4">
+
+                    <div class="col-md-4" data-aos="fade-up">
+                        <div class="service p-4 bg-base rounded-4 shadow-effect">
+                            <div class="iconbox rounded-4">
+                                <i class="las la-feather"></i>
+                            </div>
+                            <h5 class="mt-4 mb-2">Reflexion</h5>
+                            <p>Vous avez des idées pour monter un nouveau projets, mais vous ne savez pas par quoi commencer, nous pouvons y réflechir étapes par étapes.</p>
+                            <a href="#" class="link-custom">Read More</a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4" data-aos="fade-up" data-aos-delay="300">
+                        <div class="service p-4 bg-base rounded-4 shadow-effect">
+                            <div class="iconbox rounded-4">
+                                <i class="las la-pencil-ruler"></i>
+                            </div>
+                            <h5 class="mt-4 mb-2">Maquettes de projet</h5>
+                            <p>Nous pouvons ensemble voir vos besoins, je me chargerais par la suite de vous proposez différentes maquettes correspondant à vos gouts avec Figma.
+                                
+                            </p>
+                            <a href="#" class="link-custom">Read More</a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4" data-aos="fade-up" data-aos-delay="600">
+                        <div class="service p-4 bg-base rounded-4 shadow-effect">
+                            <div class="iconbox rounded-4">
+                                <i class="las la-laptop-code"></i>
+                            </div>
+                            <h5 class="mt-4 mb-2">Développement</h5>
+                            <p>Vous avez besoin de quelqu'un qui puisse designer vos services et le deploiement de vos application ? 
+                                Je me charge aussi de les concevoir avec des outils comme Flutter et Firebase.</p>
+                            <a href="#" class="link-custom">Read More</a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+        
+        <section id="work" class="full-height px-lg-5">
+            <div class="container">
+
+                <div class="row pb-4" data-aos="fade-up">
+                    <div class="col-lg-8">
+                        <h6 class="text-brand">Projets</h6>
+                        <h1>Quelques differents projets</h1>
+                    </div>
+                </div>
+
+                <div class="row gy-4">
+
+                    <div class="col-md-6" data-aos="fade-up">
+                        <div class="card-custom rounded-4 bg-base shadow-effect">
+                            <div class="card-custom-image rounded-4">
+                                <img class="rounded-4" src="./assets/images/hero_azure_v2.jpg" alt="">
+                            </div>
+                            <div class="card-custom-content p-4">
+                                <h4>Projet d'IA - FranceInter</h4>
+                                <p>Mise en place d'un systeme qui reconnais le nombre de personne sur une vidéo avec l'IA d'Azure et avertit en francais et en anglais lorsqu'il ya trop de monde dans une pièce</p>
+                            
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6" data-aos="fade-up" data-aos-delay="300">
+                        <div class="card-custom rounded-4 bg-base shadow-effect">
+                            <div class="card-custom-image rounded-4">
+                                <img class="rounded-4" src="./assets/images/pokemon.jpg" alt="">
+                            </div>
+                            <div class="card-custom-content p-4">
+                                <h4> API Jeux Pokemon</h4>
+                                <p>Mise en  d'une api pokemon où l'on pouvais fusionner des pokemons entre eux </p>
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6" data-aos="fade-up">
+                        <div class="card-custom rounded-4 bg-base shadow-effect">
+                            <div class="card-custom-image rounded-4">
+                                <img class="rounded-4" src="./assets/images/fiit.jpg" alt="">
+                            </div>
+                            <div class="card-custom-content p-4">
+                                <h4>Fitting</h4>
+                                <p>Application pouvant reperer les chaussure de son choix autour de sa position</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6" data-aos="fade-up">
+                        <div class="card-custom rounded-4 bg-base shadow-effect">
+                            <div class="card-custom-image rounded-4">
+                                <img class="rounded-4" src="./assets/images/fiit.jpg" alt="">
+                            </div>
+                            <div class="card-custom-content p-4">
+                                <h4>Fitting</h4>
+                                <p>Application pouvant reperer les chaussure de son choix autour de sa position</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6" data-aos="fade-up">
+                        <div class="card-custom rounded-4 bg-base shadow-effect">
+                            <div class="card-custom-image rounded-4">
+                                <img class="rounded-4" src="./assets/images/fiit.jpg" alt="">
+                            </div>
+                            <div class="card-custom-content p-4">
+                                <h4>Fitting</h4>
+                                <p>Application pouvant reperer les chaussure de son choix autour de sa position</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6" data-aos="fade-up" data-aos-delay="300">
+                        <div class="card-custom rounded-4 bg-base shadow-effect">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        
+        <section id="about" class="full-height px-lg-5">
+            <div class="container">
+
+                <div class="row pb-4" data-aos="fade-up">
+                    <div class="col-lg-8">
+                        <h6 class="text-brand">Mon Parcours</h6>
+                        <h1>Parcours scolaire</h1>
+                    </div>
+                </div>
+
+                <div class="row gy-5">
+                    <div class="col-lg-6">
+
+                        <h3 class="mb-4" data-aos="fade-up" data-aos-delay="300">Education</h3>
+                        <div class="row gy-4">
+
+                            <div class="col-12" data-aos="fade-up" data-aos-delay="600">
+                                <div class="bg-base p-4 rounded-4 shadow-effect">
+                                    <h4>Ecole Elemenaire</h4>
+                                    <p class="text-brand mb-2">Ecole Paul Valery (2006 - 2011)</p>
+                                    <p class="mb-0">Apprentisage des bases scolaires</p>
+                                </div>
+                            </div>
+
+                            <div class="col-12" data-aos="fade-up" data-aos-delay="600">
+                                <div class="bg-base p-4 rounded-4 shadow-effect">
+                                    <h4>Collège</h4>
+                                    <p class="text-brand mb-2">Collège Hubert Robert (2011 - 2015)</p>
+                                    <p class="mb-0">Apprentissage des méthodologies de travail</p>
+                                </div>
+                            </div>
+
+                            <div class="col-12" data-aos="fade-up" data-aos-delay="600">
+                                <div class="bg-base p-4 rounded-4 shadow-effect">
+                                    <h4>Lycée</h4>
+                                    <p class="text-brand mb-2">Lycée Geoffroy Saint-Hilaire(2015 - 2018)</p>
+                                    <p class="mb-0">Apprentissage de l'économie principalement</p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-6">
+
+                        <div class="row gy-4">
+
+                            <div class="col-12" data-aos="fade-up" data-aos-delay="600">
+                                <div class="bg-base p-4 rounded-4 shadow-effect">
+                                    <h4>Lycée</h4>
+                                    <p class="text-brand mb-2">Lycée Nikola Tesla(2018 - 2019)</p>
+                                    <p class="mb-0">Optention du Bac Générale d'Economie et Sociales</p>
+                                </div>
+                            </div>
+
+                            <div class="col-12" data-aos="fade-up" data-aos-delay="600">
+                                <div class="bg-base p-4 rounded-4 shadow-effect">
+                                    <h4>ETNA</h4>
+                                    <p class="text-brand mb-2">Ecole des technologies numériques et avancées (2020 - 2021)</p>
+                                    <p class="mb-0">Parcours préparatoire à l'ETNA. Apprentissage des base de l'informatique par différents projets, pour se familiariser au monde du travial, suivis d'une alternance en parallèle</p>
+                                </div>
+                            </div>
+                            <div class="col-12" data-aos="fade-up" data-aos-delay="600">
+                                <div class="bg-base p-4 rounded-4 shadow-effect">
+                                    <h4>ETNA</h4>
+                                    <p class="text-brand mb-2">Ecole des technologies numériques et avancées (2022 - 2023)</p>
+                                    <p class="mb-0">Parcours préparatoire à l'ETNA. Apprentissage des base de l'informatique par différents projets, pour se familiariser au monde du travial, suivis d'une alternance en parallèle</p>
+                                </div>
+                            </div>
+
+                            
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+       
+        <section id="reviews" class="full-height px-lg-5">
+            <div class="container">
+
+                <div class="row pb-4" data-aos="fade-up">
+                    <div class="col-lg-8">
+                        <h6 class="text-brand">Compétances</h6>
+                        <h1>Que dire sur moi</h1>
+                    </div>
+                </div>
+
+                <div class="row gy-4">
+
+                    <div class="col-md-4" data-aos="fade-up">
+
+                        <div class="review shadow-effect bg-base p-4 rounded-4">
+                            <div class="text-brand h5">
+                                <i class="las la-star"></i>
+                                <i class="las la-star"></i>
+                                <i class="las la-star"></i>
+                                <i class="las la-star"></i>
+                                <i class="las la-star"></i>
+                            </div>
+                            <p class="my-3">La volonté de faire et de réussir</p>
+                            <div class="person">
+                                <h5 class="mb-0">Determination</h5>
+                                
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-4" data-aos="fade-up" data-aos-delay="300">
+
+                        <div class="review shadow-effect bg-base p-4 rounded-4">
+                            <div class="text-brand h5">
+                                <i class="las la-star"></i>
+                                <i class="las la-star"></i>
+                                <i class="las la-star"></i>
+                                <i class="las la-star"></i> 
+                            </div>
+                            <p class="my-3">Se présente toujours à l'heure et est serieux dans ses taches</p>
+                            <div class="person">
+                                <h5 class="mb-0">Assiuité et serieux</h5>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-4" data-aos="fade-up" data-aos-delay="600">
+
+                        <div class="review shadow-effect bg-base p-4 rounded-4">
+                            <div class="text-brand h5">
+                                <i class="las la-star"></i>
+                                <i class="las la-star"></i>
+                                <i class="las la-star"></i>
+                                <i class="las la-star"></i>
+                                <i class="las la-star"></i>
+                            </div>
+                            <p class="my-3"> Fais preuve d'un grand sens de la Créativité</p>
+                            <div class="person">
+                                <h5 class="mb-0">Créatif</h5>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+        
+        <footer class="py-5 px-lg-5">
+            <div class="container">
+                <div class="row gy-4 justify-content-between">
+                    <div class="col-auto">
+                       
+                    </div>
+                    <div class="col-auto">
+                        <div class="social-icons">
+                            <a href="https://www.instagram.com/?hl=fr"><i class="lab la-instagram"></i></a>
+                            <a href="https://dribbble.com/caalor"><i class="lab la-dribbble"></i></a>
+                            <a href="glpat-EsTjjsB84tBz6oXbZ2ip
+                            "><i class="lab la-github"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+       
+
+    </div>
+
+
+
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/js/aos.js"></script>
+    <script src="./assets/js/main.js"></script>
+</body>
+
+</html>
+
